ed31d9a0401c51e94e4b9630bb06733c11530f2d
gary-giambatista <67201255+gary-giambatista@users.noreply.github.com>
2023-01-12 04:31:29 -0500

    Update validAnagram.js

--- validAnagram.js
+++ validAnagram.js
@@ -3,6 +3,44 @@
  * @param {string} t
  * @return {boolean}
  */
+// Solution 1 - using Maps
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+	//check lengths
+	if (s.length !== t.length) return false;
+	//create 2 maps
+	const mapS = new Map();
+	const mapT = new Map();
+
+	for (let i = 0; i < s.length; i++) {
+		//add to s map
+		if (mapS.has(s[i])) { //n(n)
+			let prevValuePlusOne = mapS.get(s[i]) + 1; //n(n)
+			mapS.set(s[i], prevValuePlusOne); //n(1)
+		} else {
+			mapS.set(s[i], 1); //n(1)
+		}
+		//add to t map
+		if (mapT.has(t[i])) { //n(n)
+			let prevValuePlusOne = mapT.get(t[i]) + 1; //n(n)
+			mapT.set(t[i], prevValuePlusOne); //n(1)
+		} else {
+			mapT.set(t[i], 1); //n(1)
+		}
+	}
+	for (let i = 0; i < s.length; i++) {
+		//check for equality of values for each key
+		if (mapS.get(s[i]) !== mapT.get(s[i])) return false;
+	}
+	return true;
+};
+
+
+// Solution 2 - using a JS object
  var isAnagram = function(s, t) {
     if (s.length !== t.length) return false
     const charToCountMap = {}

d630262fea85ce8f964fca47d46b29be4a5b8b83
gary-giambatista <67201255+gary-giambatista@users.noreply.github.com>
2023-01-14 09:36:08 -0500

    Update validPalindrome.js

--- validPalindrome.js
+++ validPalindrome.js
@@ -2,6 +2,19 @@
  * @param {string} s
  * @return {boolean}
  */
+// Solution 1 - match() is a faster regex search, but it returns an array instead of a string. To fix it, just use toLowerCase() before match().
+var isPalindrome = function(s) {
+    const chars = s.toLowerCase().match(/[a-z0-9]/g); // alphanumeric characters include numbers
+    if (chars === null) return true; // if chars is empty(match() returns null when empty), return
+    for (let i = 0; i < chars.length; i++) {
+	    if (chars[i] !== chars[chars.length - i - 1]) return false;
+	}
+	return true;
+};
+
+
+
+// Solution 2 - using replace - slightly inefficient bc needs to replace matched string with new substring
 var isPalindrome = function(s) {
     let array = s.replace(/[^a-z0-9]/gi, '').toLowerCase()
     for (let i = 0; i < array.length; i++) {

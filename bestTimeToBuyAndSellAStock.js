c43aa24785ba98018b2313f7f9f628daad173fcb
gary-giambatista <67201255+gary-giambatista@users.noreply.github.com>
2023-01-15 06:23:47 -0500

    Update bestTimeToBuyAndSellAStock.js

--- bestTimeToBuyAndSellAStock.js
+++ bestTimeToBuyAndSellAStock.js
@@ -1,11 +1,11 @@
 var maxProfit = function(prices) {
-     let buy = prices[0]
-     let profit = 0
+     let buy = prices[0] // always have to buy the first day
+     let profit = 0 // profit will always be 0 if not a good sell day
      for (let i = 0; i < prices.length; i++) {
-        if (prices[i] < buy) {
-            buy = prices[i]
+        if (prices[i] < buy) { //checking for a lower buy price
+            buy = prices[i] // if a lower buy price exists, set it as buy
         } else {
-            profit = Math.max(prices[i] - buy, profit)
+            profit = Math.max(prices[i] - buy, profit) // otherwise check to see if the current price - buy is above the current profit, if it is, set it to profit, if not keep profit
         }
      }
      return profit

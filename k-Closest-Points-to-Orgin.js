88a5f1618c3e939db0cfa8c689a997f8e13a5fb8
Gary Giambatista <67201255+gary-giambatista@users.noreply.github.com>
2023-02-27 19:41:32 -0500

    Create k-Closest-Points-to-Orgin.js

---
+++ k-Closest-Points-to-Orgin.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[][]} points
+ * @param {number} k
+ * @return {number[][]}
+ */
+var kClosest = function(points, k) {
+    const minHeap = new MinPriorityQueue();
+    for (let point of points) { // point is an array [x, y]
+        let total = point[0] ** 2 + point[1] ** 2 // square both x and y and add them together (total distance to (0,0))
+        minHeap.enqueue(point, total) // key, value: uses value to assign priority and key becomes element
+    }
+    const closestPoints = []
+    while (k-- > 0) { //loop while k is greater than 0 and k = k-1 after each loop
+        const {priority, element} = minHeap.dequeue(minHeap.front()) // priority = total, element = point [x, y]
+        closestPoints.push(element)
+    }
+    return closestPoints
+};

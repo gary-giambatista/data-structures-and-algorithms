6f79243acefff523141ab079c4b5f6322155b534
Gary Giambatista <67201255+gary-giambatista@users.noreply.github.com>
2023-03-15 11:35:58 -0400

    Update Linked List Cycle.js

--- Linked List Cycle.js
+++ Linked List Cycle.js
@@ -1,5 +1,27 @@
-//map solution
+//2 pointer fast/slow solution
+//Time complexity O(n)
+//Space complexity O(1)
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    if (!head || !head.next) return false;
+    let slow = head;
+    let fast = head;
 
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow === fast) {
+            return true;
+        }
+    }
+    return false;
+};
+
+
+//map solution
 /**
  * Definition for singly-linked list.
  * function ListNode(val) {

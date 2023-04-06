112d8d0e5a819512aea54a152c733dccdfc8892a
Gary Giambatista <67201255+gary-giambatista@users.noreply.github.com>
2023-04-05 20:20:34 -0400

    Create Evaluate Reverse Polish Notation.js

---
+++ Evaluate Reverse Polish Notation.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function(tokens) {
+    const stack = []; // initialize a stack
+    for (let token of tokens) { // check each item in the tokens array
+        if (token === "+") { // if + operator
+            stack.push(stack.pop() + stack.pop()); //pop off last 2 items, add and push back
+        } else if (token === "-") { // if - operator
+            let a = stack.pop(); //2nd element (in order)
+            let b = stack.pop(); // 1st element (in order)
+            stack.push(b - a) // push the difference between 1st - 2nd
+        } else if (token === "*") { // if * operator
+            stack.push(stack.pop() * stack.pop()); // //pop off last 2 items, multiply and push back
+        } else if (token === "/") { // if / operator
+            let a = stack.pop(); //2nd element (in order)
+            let b = stack.pop(); // 1st element (in order)
+            stack.push(Math.floor(b / a)) // X need to use parseInt() instead
+        } else stack.push(parseInt(token)) //creating stack here as integers
+    }
+    return stack[0] //return what is left in the stack [0]
+};

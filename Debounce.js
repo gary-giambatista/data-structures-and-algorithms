ee857a423e1ab805af49a6c50bac131fd69d611d
Gary Giambatista <67201255+gary-giambatista@users.noreply.github.com>
2023-05-23 22:56:55 -0400

    Create Debounce.js

---
+++ Debounce.js
@@ -0,0 +1,21 @@
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+var debounce = function(fn, t) {
+    let timeoutTimer;
+    return function(...args) {
+       clearTimeout(timeoutTimer);
+       timeoutTimer = setTimeout(() => {
+           fn(...args)
+       }, t)
+    }
+};
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
